b52b8f85c8d705877d1c5cb77b32cecee6feb8b2
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-05-29 06:30:00 -0400

    Implemented Cover With With Blur Parallax Using React Parallax, Create Custom Hook Usemenu To Load Menu Data, Displayed Different Menu Category Items With Cover

---
+++ bistro-cafe-client/src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,40 @@
+import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCatergory from '../MenuCategory/MenuCatergory';
+
+const menu = () => {
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === "popular");
+    const desserts = menu.filter(item => item.category === "dessert");
+    const soup = menu.filter(item => item.category === "soup");
+    const salad = menu.filter(item => item.category === "salad");
+    const pizza = menu.filter(item => item.category === "pizza");
+    const offered = menu.filter(item => item.category === "offered");
+
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Cafe | Menu</title>
+            </Helmet>
+            <Cover img={menuImg} title={"our menu"}></Cover>
+            {/* Main Cover */}
+            <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle>
+            {/* Offered Menu Items */}
+            <MenuCatergory items={offered}></MenuCatergory>
+            {/* desert menu items */}
+            <MenuCatergory items={desserts} title="Dessert" img={dessertImg}></MenuCatergory>
+            <MenuCatergory items={pizza} title="Pizza" img={pizzaImg}></MenuCatergory>
+            <MenuCatergory items={salad} title="Salad" img={saladImg}></MenuCatergory>
+            <MenuCatergory items={soup} title="Soup" img={soupImg}></MenuCatergory>
+        </div>
+    );
+};
+
+export default menu;
--- bistro-cafe-client/src/pages/Shared/NavBar/NavBar.jsx
+++ bistro-cafe-client/src/pages/Shared/NavBar/NavBar.jsx
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom";
 const NavBar = () => {
     const navOptions = <>
-        <li><a>Item 1</a></li>
-        <li><a>Parent</a></li>
-        <li><a>Item 3</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="">Contact Us</Link></li>
+        <li><Link to="">Dashboard</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
+        <li><Link to="">Our Shop</Link></li>
     </>
     return (
         <>
