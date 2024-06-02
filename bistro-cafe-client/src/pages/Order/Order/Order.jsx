da2467e0a534f32306535b8d3033cee7ab42f20e
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-02 06:21:49 -0400

    Added Select Right Tab Based On The Category and setup the connection to server

--- bistro-cafe-client/src/pages/Order/Order/Order.jsx
+++ bistro-cafe-client/src/pages/Order/Order/Order.jsx
@@ -5,10 +5,16 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../hooks/useMenu';
 import OrderTab from '../OrderTab/OrderTab';
+import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
+    const categories = ['salad', 'pizza', 'soup', 'dessert' , 'drinks'];
+    const { category } = useParams();
+    const initialIndex = categories.indexOf(category);
+    const [tabIndex, setTabIndex] = useState(initialIndex);
     const [menu] = useMenu();
+
     const desserts = menu.filter(item => item.category === "dessert");
     const soup = menu.filter(item => item.category === "soup");
     const salad = menu.filter(item => item.category === "salad");
@@ -16,6 +22,9 @@ const Order = () => {
     const drinks = menu.filter(item => item.category === "drinks");
     return (
         <div>
+             <Helmet>
+                <title>Bistro Cafe | Order Food</title>
+            </Helmet>
             <Cover img={orderCoverImg} title="Order Food"></Cover>
             <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                 <TabList>
