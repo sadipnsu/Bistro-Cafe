9e30e059821c7b2c70c40875509e0ab6ba48eb9d
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-06 20:45:12 -0400

    Setup Login Page And Auth Context

--- bistro-cafe-client/src/Routes/Routes.jsx
+++ bistro-cafe-client/src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
   
   export const router = createBrowserRouter([
     {
@@ -25,6 +26,10 @@ import Login from "../pages/Login/Login";
         {
           path: 'login',
           element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
         }
       ]
     }
--- bistro-cafe-client/src/pages/Login/Login.jsx
+++ bistro-cafe-client/src/pages/Login/Login.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { AuthContext } from '../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const captchaRef = useRef(null);
     const [disabled, setDisabled] = useState(true);
+    const {signIn} = useContext(AuthContext);
     useEffect( () =>{
         loadCaptchaEnginge(6);  
     }, [])
@@ -14,11 +17,19 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+        signIn(email,password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        }) 
     }
     const handleValidateCaptcha = () => {
         const user_captcha_value = captchaRef.current.value;
         if(validateCaptcha(user_captcha_value)){
             setDisabled(false);
+        }
+        else{
+            setDisabled(true);
         }
         
     }
@@ -57,6 +68,7 @@ const Login = () => {
                         <input disabled={disabled} className="btn btn-primary" type="submit" value="Login"/>
                     </div>
                 </form>
+                <p><small>New Here? <Link to="/signup">Create a New Account</Link></small></p>
             </div>
         </div>
     </div>
