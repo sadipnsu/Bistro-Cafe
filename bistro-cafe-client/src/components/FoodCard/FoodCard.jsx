3c7552b7f98f7353d984cee736d510448383a728
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-10 20:22:22 -0400

    Save Cart Data To The Server Using Axios

--- bistro-cafe-client/src/components/FoodCard/FoodCard.jsx
+++ bistro-cafe-client/src/components/FoodCard/FoodCard.jsx
@@ -1,15 +1,39 @@
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
+    const {name, image, price, recipe, _id} = item;
     const {user} = useAuth();
     const navigate = useNavigate();
+    const location = useLocation();
 
     const handleAddToCart = food =>{
         if(user && user.email){
             // to do : send cart item to database
+            console.log(user.email, food);
+            const cartItem = {
+                menuId : _id,
+                email : user.email,
+                name,
+                image,
+                price
+            }
+            axios.post('http://localhost:5000/carts', cartItem)
+            .then(res => {
+                console.log(res.data)
+                if(res.data.insertedId){
+                    Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${name} added to the your cart`,
+                    showConfirmButton: false,
+                    timer: 1500
+                    });
+                }
+            })
+        
         }
         else{
             Swal.fire({
@@ -23,7 +47,7 @@ const FoodCard = ({item}) => {
               }).then((result) => {
                 if (result.isConfirmed) {
                   //send the user to login page
-                    navigate('/login');
+                    navigate('/login', {state: {from: location}});
                 }
               });
         }
--- bistro-cafe-client/src/pages/Login/Login.jsx
+++ bistro-cafe-client/src/pages/Login/Login.jsx
@@ -12,6 +12,7 @@ const Login = () => {
     const location = useLocation();
 
     const from = location.state?.from?.pathname || "/";
+    console.log('state in the location login page', location.state);
     useEffect( () =>{
         loadCaptchaEnginge(6);  
     }, [])
