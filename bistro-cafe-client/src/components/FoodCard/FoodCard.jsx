ac00334a2cd93fd28a463d95fdb336f661be5772
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-11 08:34:24 -0400

    Show Add To Cart Count Using Refetch

--- bistro-cafe-client/src/components/FoodCard/FoodCard.jsx
+++ bistro-cafe-client/src/components/FoodCard/FoodCard.jsx
@@ -2,6 +2,7 @@ import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 const FoodCard = ({item}) => {
     const {name, image, price, recipe, _id} = item;
@@ -9,12 +10,12 @@ const FoodCard = ({item}) => {
     const navigate = useNavigate();
     const location = useLocation();
     const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
 
 
-    const handleAddToCart = food =>{
+    const handleAddToCart = () =>{
         if(user && user.email){
-            // to do : send cart item to database
-            console.log(user.email, food);
+            // send cart item to database
             const cartItem = {
                 menuId : _id,
                 email : user.email,
@@ -33,6 +34,8 @@ const FoodCard = ({item}) => {
                     showConfirmButton: false,
                     timer: 1500
                     });
+                    //refetch cart to update the cart items count
+                    refetch();
                 }
             })
         
@@ -64,7 +67,7 @@ const FoodCard = ({item}) => {
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
                     <button 
-                    onClick={() => handleAddToCart(item)}
+                    onClick={handleAddToCart}
                     className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">
                     Add to Cart</button>
                 </div>
