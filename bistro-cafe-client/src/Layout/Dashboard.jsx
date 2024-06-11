cfeabdfb30d1fe3f29753c771e29fbf613709b2f
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-11 15:43:07 -0400

    Added Cart Data From Custom Hook And Calculate Total Price

--- bistro-cafe-client/src/Layout/Dashboard.jsx
+++ bistro-cafe-client/src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaCalendar, FaHome, FaList, FaListAlt, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
@@ -6,7 +6,7 @@ const Dashboard = () => {
         <div className="flex">
             {/* dashboard side bar */}
             <div className="w-64 min-h-screen bg-orange-400">
-                <ul className="menu p-4">
+                <ul className="menu p-4 text-black">
                     <li><NavLink to="/dashboard/userHome">
                         <FaHome></FaHome>
                         User Home</NavLink>
@@ -27,10 +27,19 @@ const Dashboard = () => {
                         <FaList></FaList>
                         My Bookings</NavLink>
                     </li>
+                    <div className="divider"></div>
+                    <li><NavLink to="/">
+                        <FaHome></FaHome>
+                         Home</NavLink>
+                    </li>
+                    <li><NavLink to="/order/salad">
+                        <FaListAlt></FaListAlt>
+                         Menu</NavLink>
+                    </li>
                 </ul>
             </div>
             {/* dashboard content */}
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
