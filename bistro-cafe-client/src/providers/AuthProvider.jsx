082959c89881b240604c8053b91e5b29e981b2df
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-13 00:56:10 -0400

    Create A JWT Token And Save It On Local Storage

--- bistro-cafe-client/src/providers/AuthProvider.jsx
+++ bistro-cafe-client/src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -9,6 +10,7 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -37,9 +39,18 @@ const AuthProvider = ({children}) => {
             setUser(currentUser);
             if(currentUser){
                 //get token and store client
+                const userInfo = {email: currentUser.email}
+                axiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
+                
             }
             else{
                 //
+                localStorage.removeItem('access-token');
             }
             console.log('current user', currentUser);
             setLoading(false);
