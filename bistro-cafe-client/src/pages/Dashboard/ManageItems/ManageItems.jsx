6a2dd8287d5c4c972a2cf0c701e79c6c254cf91c
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-14 12:49:01 -0400

    Delete Menu Item With VerifyAdmin Middleware

--- bistro-cafe-client/src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ bistro-cafe-client/src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,12 +1,40 @@
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
 
-    const [menu] = useMenu();
-    const handleDeleteItem = (item) => {  
+    const [menu, , refetch] = useMenu();
+    const axiosSecure = useAxiosSecure();
 
+    const handleDeleteItem = (item) => {  
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then( async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${item._id}`)
+                //console.log(res.data);
+                if(res.data.deletedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${item.name} has been deleted`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      }); 
+                }
+            
+            }
+          });
     }
     return (
         <div>
@@ -45,7 +73,7 @@ const ManageItems = () => {
                                     <td>{item.name}</td>
                                     <td>${item.price}</td>
                                     <td>
-                                        <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost btn-sm bg-orange-500">
+                                        <button className="btn btn-ghost btn-sm bg-orange-500">
                                             <FaEdit className="text-white text-1xl"></FaEdit>
                                         </button>
                                     </td>
