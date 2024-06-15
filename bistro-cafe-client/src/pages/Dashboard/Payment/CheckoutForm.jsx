df6074b323d2299830cbe5f509a093009fedff9e
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-15 01:24:03 -0400

    Use Confirm Card Payment To Process Payment

--- bistro-cafe-client/src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ bistro-cafe-client/src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,13 +2,16 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckoutForm = () => {
     const [error, setError] = useState('');
     const [clientSecret, setClientSecret] = useState('');
+    const [transactionId, setTransactionId] = useState('');
     const stripe = useStripe();
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
+    const {user} = useAuth();
     const [cart] = useCart();
     const Totalprice = cart.reduce( (total, item) => total + item.price, 0 )
 
@@ -44,6 +47,28 @@ const CheckoutForm = () => {
             console.log('Payment method', paymentMethod);
             setError('');
         }
+
+        //Confirm Payment
+        const { paymentIntent, error: confirmError }  = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous'
+
+                }
+            }
+        })
+        if(confirmError){
+            console.log('confirm error');
+        }
+        else{
+            console.log('payment intent', paymentIntent);
+            if(paymentIntent.status === 'succeeded'){
+                console.log('transaction id', paymentIntent.id);
+                setTransactionId(paymentIntent.id);
+            }
+        }
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -68,6 +93,7 @@ const CheckoutForm = () => {
         Pay
       </button>
       <p className="text-red-600">{error}</p>
+      {transactionId && <p className="text-green-600">Your transaction id: {transactionId}</p>}
         </form>
     );
 };
