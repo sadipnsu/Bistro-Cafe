c4ed2568de66369965b92000b8b8b08945b82dda
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-15 12:39:34 -0400

    Different Dashboard Home Page For User And Admin

--- bistro-cafe-client/src/Routes/Routes.jsx
+++ bistro-cafe-client/src/Routes/Routes.jsx
@@ -16,6 +16,8 @@ import ManageItems from "../pages/Dashboard/ManageItems/ManageItems";
 import UpdateItem from "../pages/Dashboard/UpdateItem/UpdateItem";
 import Payment from "../pages/Dashboard/Payment/Payment";
 import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
+import UserHome from "../pages/Dashboard/UserHome/UserHome";
+import AdminHome from "../pages/Dashboard/AdminHome/AdminHome";
   
   export const router = createBrowserRouter([
     {
@@ -54,6 +56,10 @@ import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
       children: [
         // Normal user routes
         {
+          path:'userHome',
+          element: <UserHome></UserHome>
+        },
+        {
           path:'cart',
           element: <Cart></Cart>
         },
@@ -67,6 +73,10 @@ import PaymentHistory from "../pages/Dashboard/PaymentHistory/PaymentHistory";
         },
 
         //admin only routes
+        {
+          path: 'adminHome',
+          element: <AdminRoute><AdminHome></AdminHome></AdminRoute>
+        },
         {
           path: 'addItems',
           element: <AdminRoute><AddItems></AddItems></AdminRoute>
