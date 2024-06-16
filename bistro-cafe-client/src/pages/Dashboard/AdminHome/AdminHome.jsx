fa95b23215fe982ea33d0d45027e2e519f794f75
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2024-06-16 12:06:27 -0400

    Load Data And Display Stats On The Admin Home

--- bistro-cafe-client/src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ bistro-cafe-client/src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,15 +1,66 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaUser } from "react-icons/fa";
 
 const AdminHome = () => {
     const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const {data: stats} = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/admin-stats');
+            return res.data;
+        }
+    })
+
     return (
         <div>
             <h2 className="text-3xl">
-                <span>Hi, Welcome</span>
+                <span>Hi, Welcome </span>
                 {
                     user?.displayName ? user.displayName : 'Back'
                 }
             </h2>
+            <div className="stats shadow mx-16 px-12">
+  
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaDollarSign className="text-3xl"></FaDollarSign>
+                    </div>
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value">${stats?.revenue}</div>
+                    <div className="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+                
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaUser className="text-3xl"></FaUser>
+                    </div>
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value">{stats?.users}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaBook className="text-3xl"></FaBook>
+                    </div>
+                    <div className="stat-title">Menu Items</div>
+                    <div className="stat-value">{stats?.menuItems}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+                
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                    </div>
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{stats.orders}</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+            </div>
         </div>
     );
 };
--- bistro-cafe-server/index.js
+++ bistro-cafe-server/index.js
@@ -236,19 +236,30 @@ async function run() {
     });
 
     //stats or analytics api
-    app.get('/admin-stats', async (req, res) => {
+    app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
       const users = await userCollection.estimatedDocumentCount();
       const menuItems = await menuCollection.estimatedDocumentCount();
       const orders = await paymentCollection.estimatedDocumentCount();
 
-      const payments = await paymentCollection.find().toArray();
-      const revenue = payments.reduce((total, payment) => total + payment.price, 0) 
-
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce((total, payment) => total + payment.price, 0) 
+      const result = await paymentCollection.aggregate([
+        {
+          $group:{
+            _id: null,
+            totalRevenue:{
+              $sum: '$price'
+            }
+          }
+        }
+      ]).toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 
       res.send({
         users,
         menuItems,
-        orders,revenue
+        orders,
+        revenue
       });
     })
 
